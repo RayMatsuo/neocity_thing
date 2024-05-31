663d20a08757652b14c96735cd6e7624efa15b3f
marcie <mookofski7@gmail.com>
2024-05-31 23:04:31 +0900

    help me

--- js/cell.js
+++ js/cell.js
@@ -6,14 +6,19 @@ var resolution = new Vector2(
 var grid_resolution = 200;
 
 var cry=[
-  '<span style="color:red">help</span>',
+  '<span>help</span>',
   "i'm tired",
   "I don't want to keep going",
-  "why am i alive",
+  "why am i <p>alive</p>",
   "i can't keep paying for this",
   "please someone hold me",
   "please someone keep me safe for once",
   "i'm tired of fighting for someone",
+  "im in a hell of <span>my own making</span>",
+  "help me <p>help me</p>",
+  "please keep me safe",
+  "what even is <span>comfort</span>",
+  "i dont know what <span>comfort</span> is",
 ]
 
 class Cell {
